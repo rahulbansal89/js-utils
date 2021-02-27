344eca93fdcf4fe81f98ec5d846458e3acf76efd
Rahul Bansal <rahul.bansal@ofbusiness.in>
2021-02-27 22:25:53 +0530

    added some helpers

---
+++ src/helper.js
@@ -0,0 +1,108 @@
+const Utils = require(".");
+const { SETTING_TYPES, AVAILABILITY, DAYS_MAP } = require("./constants");
+
+const Helper = {
+    isStoreActive: ({ settings = {} } = {}) => {
+        if (Utils.isUndefinedOrNullOrEmptyObject(settings)) {
+            return true;
+        }
+        return settings[SETTING_TYPES.STORE_STATUS] === AVAILABILITY.YES;
+    },
+    isDeliveryActive: ({ settings = {} } = {}) => {
+        if (Utils.isUndefinedOrNullOrEmptyObject(settings)) {
+            return true;
+        }
+        return settings[SETTING_TYPES.STORE_DELIVERY_STATUS] === AVAILABILITY.YES;
+    },
+    checkIfStoreOpen: ({ settings = {} } = {}) => {
+        if (!Helper.isStoreActive({ settings })) {
+            return false;
+        }
+        if (Utils.isUndefinedOrNullOrEmptyObject(settings)) {
+            return true;
+        }
+        if (!settings[SETTING_TYPES.BUSINESS_HOURS]) {
+            return true;
+        }
+        const currHour = new Date().getHours();
+        const { open, close, days } = settings[SETTING_TYPES.BUSINESS_HOURS];
+        if (Utils.isUndefinedOrNullOrEmpty(open) || Utils.isUndefinedOrNullOrEmpty(close)) {
+            return false;
+        }
+        const inHours = currHour >= open && currHour <= close;
+        if (Utils.isUndefinedOrNullOrEmptyList(days) && inHours) {
+            return true;
+        }
+        const currDay = new Date().getDay();
+        const inDays = days.indexOf(DAYS_MAP[currDay]) !== -1;
+        if (inDays && inHours) {
+            return true;
+        }
+        return false;
+    },
+    checkIfDeliveryOpen: (supplier = {}) => {
+        if (!Helper.isDeliveryActive(supplier)) {
+            return false;
+        }
+        if (Utils.isUndefinedOrNullOrEmptyObject(supplier)) {
+            return false;
+        }
+        const { settings = {} } = supplier;
+        // if no settings then always open
+        if (Utils.isUndefinedOrNullOrEmptyObject(settings)) {
+            return true;
+        }
+        // if no delivery settings then always open
+        if (!settings[SETTING_TYPES.DELIVERY_TIME]) {
+            return true;
+        }
+        const currHour = new Date().getHours();
+        const { open, close } = settings[SETTING_TYPES.DELIVERY_TIME];
+        if (Utils.isUndefinedOrNullOrEmpty(open) || Utils.isUndefinedOrNullOrEmpty(close)) {
+            return false;
+        }
+        const inHours = currHour >= open && currHour <= close;
+        if (inHours) {
+            return true;
+        }
+        return false;
+    },
+    checkIfDeliveryServicable: (supplier = {}, selectedLocation = {}) => {
+        if (Utils.isUndefinedOrNullOrEmptyObject(supplier)) {
+            return false;
+        }
+        const { settings = {} } = supplier;
+        // if no settings then always open
+        if (Utils.isUndefinedOrNullOrEmptyObject(settings)) {
+            return true;
+        }
+        // if no restricted delivery setting
+        if (!settings[SETTING_TYPES.RESTRITCTED_DELIVERY]) {
+            return true;
+        }
+        // if restricted delivery is here
+        // then check if distance is valid
+        const { availability, range } = settings[SETTING_TYPES.RESTRITCTED_DELIVERY];
+        if (availability === AVAILABILITY.YES) {
+            const distance = parseInt(Helper.getDistance(supplier, selectedLocation));
+            const restrictedDelivery = parseInt(range);
+            if (distance && restrictedDelivery >= distance) {
+                return true;
+            }
+            return false;
+        }
+        return false;
+    },
+    getDistance: (supplier = {}, selectedLocation = {}) => {
+        if (Utils.isUndefinedOrNullOrEmptyObject(supplier) || Utils.isUndefinedOrNullOrEmptyObject(selectedLocation)) {
+            return '';
+        }
+    
+        return Utils.getDistanceFromLatLonInKm(
+            supplier.supplierProfile.location && supplier.supplierProfile.location.coordinates,
+            selectedLocation && selectedLocation.coordinates,
+        ) || '';
+    },
+};
+
+module.exports = Helper;
